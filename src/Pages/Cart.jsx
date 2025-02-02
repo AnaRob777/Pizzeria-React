4d3e1e30e08c8045dae22dcc280fa45d59d09043
AnaRob777 <106563798+AnaRob777@users.noreply.github.com>
2025-02-02 14:12:57 -0300

    hito 6

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,40 +1,32 @@
-import React, { useState } from "react";
-import { pizzaCart } from "../pizzas";
-
+import React from "react";
+import { useCarrito } from "../context/CartContext"; 
 const Cart = () => {
-  const [cart, setCart] = useState(pizzaCart);
+  const { carrito, eliminarDelCarrito, actualizarCantidadProducto, calcularPrecioTotal } = useCarrito(); // Usamos el contexto
 
   const aumentarCantidad = (id) => {
-    const updatedCart = cart.map((item) =>
-      item.id === id ? { ...item, count: item.count + 1 } : item
-    );
-    setCart(updatedCart);
+    actualizarCantidadProducto(id, 1); 
   };
 
-  const disminuirCantidad = (id) => {
-    const updatedCart = cart
-      .map((item) =>
-        item.id === id ? { ...item, count: Math.max(item.count - 1, 0) } : item
-      )
-      .filter((item) => item.count > 0);
-    setCart(updatedCart);
+  const disminuirCantidad = (id, cantidad) => {
+    if (cantidad === 1) {
+      eliminarDelCarrito(id); 
+    } else {
+      actualizarCantidadProducto(id, -1); 
+    }
   };
-
-  const calculoTotal = () =>
-    cart.reduce((total, item) => total + item.price * item.count, 0);
 
   return (
     <div className="container my-5">
       <h2 className="text-center mb-4">Detalle del pedido</h2>
-      {cart.length > 0 ? (
+      {carrito.length > 0 ? (
         <div className="row">
           <div className="col-12">
             <table className="table table-bordered table-hover">
               <thead className="thead-light">
-
+               
               </thead>
               <tbody>
-                {cart.map((pizza) => (
+                {carrito.map((pizza) => (
                   <tr key={pizza.id}>
                     <td>
                       <img
@@ -48,19 +40,18 @@ const Cart = () => {
                       />
                     </td>
                     <td>{pizza.name}</td>
-                    <td>${pizza.price.toLocaleString()}</td>
-                    <td>{pizza.count}</td>
-                    <td>${(pizza.price * pizza.count).toLocaleString()}</td>
+                    <td>{pizza.cantidad}</td>
+                    <td>${(pizza.price * pizza.cantidad).toLocaleString()}</td>
                     <td>
                       <button
                         className="btn btn-success btn-sm mx-1"
-                        onClick={() => aumentarCantidad(pizza.id)}
+                        onClick={() => aumentarCantidad(pizza.id)} 
                       >
                         +
                       </button>
                       <button
                         className="btn btn-danger btn-sm mx-1"
-                        onClick={() => disminuirCantidad(pizza.id)}
+                        onClick={() => disminuirCantidad(pizza.id, pizza.cantidad)} 
                       >
                         -
                       </button>
@@ -71,7 +62,7 @@ const Cart = () => {
             </table>
           </div>
           <div className="col-12 text-end">
-            <h4>Total: ${calculoTotal().toLocaleString()}</h4>
+            <h4>Total: ${calcularPrecioTotal().toLocaleString()}</h4>
             <button className="btn btn-primary mt-3">Pagar</button>
           </div>
         </div>
--- src/components/Cardpizza.jsx
+++ src/components/Cardpizza.jsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { useCarrito } from "../context/CartContext.jsx"; 
 
-const CardPizza = ({ name, price, ingredients, img, desc}) => {
+const CardPizza = ({ name, price, ingredients, img, desc, id }) => {
+  const { agregarAlCarrito } = useCarrito(); 
+
+  
+  const pizza = {
+    id,
+    name,
+    price,
+    ingredients,
+    img,
+    desc,
+    cantidad: 1,
+  };
+
   return (
     <div className="col-md-4 mt-4">
       <div className="card mb-4">
         <img src={img} className="card-img-top" alt={name} />
         <div className="card-body">
-          <h3 className="card-title"> <strong>{name}</strong></h3>
-          <br/>
+          <h3 className="card-title">
+            <strong>{name}</strong>
+          </h3>
           <p className="card-text">{desc}</p>
-          <br/>
           <ul className="list-unstyled mb-3">
             <strong>Ingredientes:</strong>
             {ingredients.map((ingredient, index) => (
@@ -18,13 +32,17 @@ const CardPizza = ({ name, price, ingredients, img, desc}) => {
               </li>
             ))}
           </ul>
-            <br />
           <h5 className="card-text">
             <strong>Precio: ${price.toLocaleString()}</strong>
           </h5>
           <div className="d-flex justify-content-between">
             <button className="btn btn-light border-dark">Ver Más 👀</button>
-            <button className="btn btn-dark">Añadir 🛒</button>
+            <button
+              className="btn btn-dark"
+              onClick={() => agregarAlCarrito(pizza)} 
+            >
+              Añadir 🛒
+            </button>
           </div>
         </div>
       </div>
